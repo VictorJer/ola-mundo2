c4b7fee66f52d3939ea65a49b37a5aad35b41fe2
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-03 15:18:09 -0300

    adicionados arquivos de projeto iniciais

---
+++ ola mundo ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("oi");
+Console.WriteLine("------------------------");
+Console.WriteLine("oi novamente");
+Console.WriteLine("------------------------");
+
+Console.ReadLine();
+
+Console.WriteLine("oi");
